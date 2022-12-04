ab4361b9f9f45e3c3dd8bc8811582b56053a8225
sachieko <arashikabane@gmail.com>
2022-12-04 04:03:07 -0700

    Implement gallery functionality with sample array of images

--- react-frontend/src/Components/Gallery.tsx
+++ react-frontend/src/Components/Gallery.tsx
@@ -1,11 +1,24 @@
+import { useState } from 'react';
 import sampleImage from '../img/test1.jpg';
 import './Gallery.scss';
+import GalleryPicture from './GalleryPicture';
+
+const images: string[]= [];
+
+for (let i = 0; i < 6; i++) {
+  images.push(sampleImage);
+}
 
 const Gallery = () => {
+  const [imgFocus, setImgFocus] = useState<number | null>(null);
+
+  const imageList = images.map((image: string, index: number) => {
+    return <GalleryPicture isFocus={index === imgFocus} setFocus={setImgFocus} img={image} key={index} index={index} />
+  });
 
   return (
   <div className={'gallery'}>
-    <div className='img'><img src={sampleImage} alt='character'/></div>
+    {imageList}
   </div>
   );
 };
