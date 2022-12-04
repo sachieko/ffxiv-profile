1fe10d6e5fe29b51716e0606d6616f6da93b9881
sachieko <arashikabane@gmail.com>
2022-12-03 17:34:09 -0700

    Refactor conditional rendering code

--- react-frontend/src/Components/MainView.tsx
+++ react-frontend/src/Components/MainView.tsx
@@ -4,14 +4,23 @@ import OutOfChar from "./OutOfChar";
 import Wipview from "./Wipview";
 import { MenuCtx } from "../Providers/MenuContext";
 
+interface IcontentView {
+  [key: string]: JSX.Element;
+}
+
 const MainView = () => {
   const [mode] = MenuCtx();
+
+  const contentView: IcontentView = {
+    profile: <Profile />,
+    gallery: <Gallery />,
+    ooc: <OutOfChar />,
+    view4: <Wipview />
+  };
+
   return (
     <section className='main-content'>
-      {mode.mode === 'profile' && <Profile />}
-      {mode.mode === 'gallery' && <Gallery />}
-      {mode.mode === 'ooc' && <OutOfChar />}
-      {mode.mode === 'view4' && <Wipview />}
+      {contentView[`${mode.mode}`]}
     </section>
   );
 };
