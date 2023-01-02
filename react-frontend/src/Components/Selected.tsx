912be18450a720e75f2516a2d6f62a8a71c9ac9f
sachieko <arashikabane@gmail.com>
2023-01-02 00:46:25 -0700

    Create interface for Selected widget view

---
+++ react-frontend/src/Components/Selected.tsx
@@ -0,0 +1,13 @@
+interface ISelectedProps {
+  title: string;
+  text: string;
+};
+
+const Selected = ({title, text}: ISelectedProps) => {
+  return (
+  <>
+  </>
+  );
+};
+
+export default Selected;
