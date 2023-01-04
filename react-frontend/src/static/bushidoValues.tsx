a6b8632726c190efd3c2918f198fdb590e129f77
sachieko <arashikabane@gmail.com>
2023-01-04 03:10:57 -0700

    Create static file for lengthy text descriptions

---
+++ react-frontend/src/static/bushidoValues.tsx
@@ -0,0 +1,64 @@
+const meiyoKanji = '名誉';
+const makotoKanji = '誠';
+const reiKanji = '礼';
+const yuKanji = '勇';
+const jinKanji = '仁';
+const chuugiKanji = '忠義';
+const giriKanji = '義理';
+
+
+const meiyoSubtitle = 'Honor';
+const makotoSubtitle = 'Sincerity';
+const reiSubtitle = 'Respect';
+const yuSubtitle = 'Courage';
+const jinSubtitle = 'Compassion';
+const chuugiSubtitle = 'Loyalty';
+const giriSubtitle = 'Duty';
+
+const meiyoText = ``;
+const makotoText = ``;
+const reiText = ``;
+const yuText = ``;
+const jinText = ``;
+const chuugiText = ``;
+const giriText = ``;
+
+const bushidoValues = {
+  meiyo: {
+    kanji: meiyoKanji,
+    subtitle: meiyoSubtitle,
+    text: meiyoText
+  },
+  makoto: {
+    kanji: makotoKanji,
+    subtitle: makotoSubtitle,
+    text: makotoText
+  },
+  rei: {
+    kanji: reiKanji,
+    subtitle: reiSubtitle,
+    text: reiText
+  },
+  yu: {
+    kanji: yuKanji,
+    subtitle: yuSubtitle,
+    text: yuText
+  },
+  jin: {
+    kanji: jinKanji,
+    subtitle: jinSubtitle,
+    text: jinText
+  },
+  chuugi: {
+    kanji: chuugiKanji,
+    subtitle: chuugiSubtitle,
+    text: chuugiText
+  },
+  giri: {
+    kanji: giriKanji,
+    subtitle: giriSubtitle,
+    text: giriText
+  }
+};
+
+export default bushidoValues;
