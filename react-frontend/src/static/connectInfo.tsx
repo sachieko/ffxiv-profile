ef20e048d7bbdbc20fad17469a6224e30d24db59
sachieko <arashikabane@gmail.com>
2023-01-04 19:58:50 -0700

    Add static files for connect and OOC info

---
+++ react-frontend/src/static/connectInfo.tsx
@@ -0,0 +1,15 @@
+interface IconnectItem {
+  title: string;
+  subtitle: string;
+  text: string;
+};
+
+const connectInfo: IconnectItem[] = [
+  {
+    title: 'Connect',
+    subtitle: 'Connect Subtitle',
+    text: ''
+  }
+];
+
+export default connectInfo;
