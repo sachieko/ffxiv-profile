4ebfb4a142f1e35339dd311669a538a0d284954b
sachieko <arashikabane@gmail.com>
2023-01-11 03:46:58 -0700

    Overhaul styles for responsive views. Fix height/width errors of app for all views.

--- react-frontend/src/App.tsx
+++ react-frontend/src/App.tsx
@@ -6,7 +6,7 @@ import { MenuProvider } from './Providers/MenuContext'
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
     <MenuProvider>
       <Menu message={'Nagisa Shibata 柴田の凪沙'} menuLinks={['profile', 'gallery', 'connect', 'ooc']} ></Menu>
       <MainView />
